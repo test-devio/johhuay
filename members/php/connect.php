8ba6be93303c3beacd48c78a0fb69a5c3310b183
test-devio <108274427+test-devio@users.noreply.github.com>
2023-02-02 17:14:57 +0700

    Update connect.php

--- members/php/connect.php
+++ members/php/connect.php
@@ -1,7 +1,7 @@
 <?php 
     session_start();
     // $conn = new mysqli('localhost', 'scourses_1plus', '48Rv9YKYiu', 'scourses_1plus');
-    $conn = new mysqli('localhost', 'root','3fmEXU5Wgt' ,'qxubhdwxqw'); // 'Hostname', 'Username', 'Password', 'Db name';
+    $conn = new mysqli('206.189.46.156', 'qxubhdwxqw','3fmEXU5Wgt' ,'qxubhdwxqw'); // 'Hostname', 'Username', 'Password', 'Db name';
     $conn->set_charset("utf8");
     if ($conn->connect_errno){
         die("Connection Failed! ".$conn->connect_error);
